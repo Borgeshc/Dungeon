0388bf028612553fa144c073f9b71dec3d1d24c0
Borgeshc <hborges@digitaliris.com>
2016-11-23 13:40:03 -0600

    Updated menu and add choosing of classes

---
+++ Assets/ChooseClass.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChooseClass : MonoBehaviour
+{
+    public static int classChosen;
+
+    public void Warrior()
+    {
+        classChosen = 1;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Berserker()
+    {
+        classChosen = 2;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Archer()
+    {
+        classChosen = 3;
+        SceneManager.LoadScene("Game");
+    }
+}
